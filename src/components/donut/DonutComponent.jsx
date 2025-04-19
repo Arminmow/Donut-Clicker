109c8e0eaa2006ced9b683d15e810641120f919b
Armin <arminmow@gmail.com>
2025-04-19 23:19:39 +0330

    Removed Header from mobile

--- src/components/donut/DonutComponent.jsx
+++ src/components/donut/DonutComponent.jsx
@@ -7,7 +7,7 @@ const donutImage =
 const DonutComponent = () => {
   return (
     <div className="relative flex h-10/12 w-full flex-col items-center justify-center">
-      <div className="flex flex-col gap-5 md:h-1/6 h-1/5 w-full items-center justify-center pt-30">
+      <div className="flex flex-col gap-5 md:h-1/6 h-1/5 w-full items-center justify-center">
         <div
           className="text-white text-center py-2 px-4 rounded-lg text-lg"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
@@ -21,12 +21,12 @@ const DonutComponent = () => {
           0 Donuts
         </div>
       </div>
-      <div className="flex h-4/5 md:h-5/6 w-full items-center justify-center ">
+      <div className="flex h-4/5 md:h-5/6 w-full items-center justify-center md:transform md:-translate-y-5">
         <motion.img
           src={donutImage}
           alt="Donut"
           draggable={false}
-          className="h-65 w-65 md:h-80 md:w-80 object-contain rounded-full cursor-pointer"
+          className="h-70 w-70 md:h-80 md:w-80 object-contain rounded-full cursor-pointer"
           whileHover={{ scale: 1.1 }}
           whileTap={{
             scale: 0.85,
--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderComponent = () => {
   return (
-    <header className="h-2/12 w-full flex items-center justify-center border-2 border-gray-500 box-border">
+    <header className="hidden md:h-2/12 w-full md:flex items-center justify-center border-2 border-gray-500 box-border">
       <h1>Header</h1>
     </header>
   );
