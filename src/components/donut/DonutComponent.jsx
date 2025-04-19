b6edec779c27ee2f5303b77648b7310ffaab3c85
Armin <arminmow@gmail.com>
2025-04-19 19:08:40 +0330

    Donut itself + Stats

--- src/components/donut/DonutComponent.jsx
+++ src/components/donut/DonutComponent.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+
+const donutImage = "https://i.ibb.co/b06nHLQ/cartoon-donut-vector-7272383-removebg-preview.png";
+
 const DonutComponent = () => {
   return (
-    <div className="flex h-10/12 w-full items-center justify-center">
-      <button className="bg-pink-500 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-400">
-        Click Me
-      </button>
+    <div className="relative flex h-10/12 w-full flex-col items-center justify-center">
+      <div className="flex flex-col gap-5 md:h-1/6 h-1/5 w-full items-center justify-center pt-30" >
+      <div
+        className="text-white text-center py-2 px-4 rounded-lg"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+      >
+        Redux Bakery
+      </div>
+        <div
+          className="w-full text-white text-center py-4 px-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+        >
+          0 Donuts
+        </div>
+      </div>
+      <div className="flex h-4/5 md:h-5/6 w-full items-center justify-center">
+        <img src={donutImage} alt="Donut" className="h-80 w-80 object-contain" />
+      </div>
     </div>
   );
 };
 
-export default DonutComponent;
+export default DonutComponent;  
