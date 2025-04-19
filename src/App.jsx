03f2d5bddb407895bfd827d00f6087d4c1085373
Armin <arminmow@gmail.com>
2025-04-19 19:36:41 +0330

    Black Gradient

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,15 @@ function App() {
         backgroundImage: `url('https://cookieclicker.wiki.gg/images/9/94/BgPink.jpg?8f1a88=&format=original')`,
       }}
     >
+      <div
+        style={{
+          backgroundImage: `url('https://orteil.dashnet.org/cookieclicker/img/blackGradient.png')`,
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: "400px",
+        }}
+      ></div>
       <GameComponent />
       <ShopComponent />
     </div>
