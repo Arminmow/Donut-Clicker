27535931269e7ea6b0bfbcd7b4ac3e866a01e3cc
Armin <arminmow@gmail.com>
2025-04-20 11:59:05 +0330

    added divider

--- src/components/shop/ShopComponent.jsx
+++ src/components/shop/ShopComponent.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
 import MenuComponent from "../menu/MenuComponent";
 
 const ShopComponent = () => {
   return (
     <>
-    <MenuComponent/>
+      <MenuComponent />
+      <div className=" w-5 bg-[url('https://orteil.dashnet.org/cookieclicker/img/panelVertical.png?v=2')]"></div>
+      <div className="bg-amber-600 h-full w-1/3 flex-col items-center justify-center hidden md:flex md:items-center md:justify-center z-10"></div>
     </>
   );
 };
