67dc2acfd3375e9a5a1b64f80854e2c6d347f2e4
Armin <arminmow@gmail.com>
2025-04-22 10:22:47 +0330

    added upgrades data

---
+++ src/data/upgrades.js
@@ -0,0 +1,28 @@
+export const Upgrades = {
+  clickPower: {
+    id: "0",
+    label: "Click Power",
+    count: 1,
+    basePrice: 50,
+    priceMultiplier: 1.15,
+    type: "active",
+  },
+  grandma: {
+    id: "1",
+    label: "Grandma",
+    count: 0,
+    basePrice: 100,
+    priceMultiplier: 1.15,
+    type: "passive",
+    cps: 0.5,
+  },
+  factory: {
+    id: "2",
+    label: "Factory",
+    count: 0,
+    basePrice: 1000,
+    priceMultiplier: 1.15,
+    type: "passive",
+    cps: 10,
+  },
+};
