41aacb8852731c62b00cc2df68c7f541c06d1531
Armin <arminmow@gmail.com>
2025-04-22 17:39:42 +0330

    added more upgrades

--- src/data/upgrades.js
+++ src/data/upgrades.js
@@ -1,22 +1,45 @@
+import { label } from "motion/react-client";
+
 export const Upgrades = [
   {
-    id: "0",
+    id: 0,
     label: "Click Power",
     basePrice: 15,
     priceMultiplier: 1.15,
   },
   {
-    id: "1",
+    id: 1,
     label: "Grandma",
     basePrice: 100,
     priceMultiplier: 1.15,
     cps: 1,
   },
   {
-    id: "3",
+    id: 3,
     label: "Farm",
     basePrice: 1100,
     priceMultiplier: 1.15,
     cps: 8,
+  },
+  {
+    id: 6,
+    label: "Mine",
+    basePrice: 12000,
+    priceMultiplier: 1.15,
+    cps: 47,
+  },
+  {
+    id: 7,
+    label: "Factory",
+    basePrice: 130000,
+    priceMultiplier: 1.15,
+    cps: 260,
+  },
+  {
+    id: 8,
+    label: "Bank",
+    basePrice: 1400000,
+    priceMultiplier: 1.15,
+    cps: 1400,
   },
 ];
